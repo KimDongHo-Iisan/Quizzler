b399774ae49772bf0c6244b19ab3493d71fb02b0
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-07-04 16:29:15 +0900

    구조체 적용

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Dongho kim on 2021/07/04.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,11 +15,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    // 참,거짓을 확인하기 위해, 2차원 배열로 변경
+    // 2차원 배열의 복잡함을 구조체를 이용해서 간단한 코드로 변화 시켜주었다.
     let quiz = [
-        ["Four + Two is equal to Six.","True"],
-        ["Five - Three is greater than One","False"],
-        ["Three + Eight is less than Ten","False"],
+        Question(text: "Four + Two is equal to Six.", answer: "True"),
+        Question(text: "Five - Three is greater than One", answer: "False"),
+        Question(text: "Three + Eight is less than Ten", answer: "False")
     ]
     
     var questionNumber = 0
@@ -29,8 +29,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        let userAnswer = sender.currentTitle // 사용자가 고른 답 -> 현재 누른 버튼
-        let actualAnswer = quiz[questionNumber][1] // 실제 답은 2차원 배열의 2번째에 있는 문자열이 답
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber].answer // 실제 답
         
         if userAnswer == actualAnswer { // 사용자가 고른 답과 실제 답이 맞다면,
             print("Right") //참을 출력
@@ -38,15 +38,17 @@ class ViewController: UIViewController {
             print("Wrong") // 아니면 거짓
         }
         
-        if questionNumber < quiz.count {
-            questionNumber += 1
+        if questionNumber + 1 < quiz.count { // 퀴즈 번호가 퀴즈 배열의 개수보다 작으면
+            questionNumber += 1 // 번호를 1 증가 -> 퀴즈번호 + 1을 해야 배열 3을 넘어가지 않게 할수 있다.
+        } else {
+            questionNumber = 0 // 아니라면 0으로 가서 처음부터 다시 시작하도록 함.
         }
 
         updateUI()
     }
     
     func updateUI() {
-        questionLabel.text = quiz[questionNumber][0] //[0]을 붙여줘야 질문에 접근할수 있음.
+        questionLabel.text = quiz[questionNumber].text // 퀴즈의 질문
     }
 }
 
