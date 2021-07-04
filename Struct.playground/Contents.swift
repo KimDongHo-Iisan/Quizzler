60a2a828f959904a7d6fd844556e0fcba659173d
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-07-04 15:46:58 +0900

    struct 2

--- Struct.playground/Contents.swift
+++ Struct.playground/Contents.swift
@@ -1,22 +1,27 @@
 
 struct Town {
-    let name = "Angelaland"
-    var citizens = ["Angela" , "Jack"]
-    var resources = ["Grain" : 100, "Ore": 42, "Wool" : 75]
+    let name: String
+    var citizens: [String]
+    var resources: [String: Int]
+    
+    //이니셜라이저를 통해 청사진에 있던 속성들을 객체로 끄집어 내어 생성하는 것이다.
+    init(name: String, citizens: [String], resources: [String: Int]) {
+        self.name = name // self를 붙이는 이유: 우리는 name = name이 Town 구조체의 속성인 name을 의미 하는것이라고 생각하지만, 컴퓨터는 그리 생각하지 못한다 그래서 지정을 해줘야 한다.
+        self.citizens = citizens
+        self.resources = resources
+    }
 
-    // 함수와 메소드는 동일한 개념이다. 하지만, 차이점은 메소드는 구조체와 클래스 내부에 정의 되어서 사용된다는 점과, 함수는 일종의 독립형으로 활동한다는 차이점이 있다.
     func fortify() {
         print("Defences increased!")
     }
 }
 
-var myTown = Town() // myTown에 Town 구조체의 속성을 복사
 
-print(myTown.citizens) // 이렇게 Town의 속성인 citizens를 myTown에서 사용할수 있음.
-print("\(myTown.name) has \(myTown.resources["Grain"]!) bags of grain.")
+var anotherTown = Town(name: "Nameless Island", citizens: ["Tom Hanks"], resources: ["Coconuts": 100])
 
-myTown.citizens.append("keaun") // 이렇게 추가도 가능하다.
-print(myTown.citizens.count)
+var ghostTown = Town(name: "Ghost", citizens: [], resources: ["Tom":1])
 
-myTown.fortify()
+anotherTown.citizens.append("Willy")
+ghostTown.fortify()
 
+// 이니셜라이저를 통해, 속성은 공유하지만, 전혀 다른 객체들을 만들수 있다는것을 알수 있다. 
